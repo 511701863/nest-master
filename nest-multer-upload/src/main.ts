f84f8c8196632190396fff4467cf96103bd35e8f
511701863 <qian.cao@nx-engine.com>
2024-01-11 14:48:32 +0800

    fix: 自定义logger系统日志

--- nest-multer-upload/src/main.ts
+++ nest-multer-upload/src/main.ts
@@ -13,11 +13,7 @@ async function bootstrap() {
 }
 bootstrap();
 // 日志打印可以用 Nest 的 Logger，它支持在创建应用的时候指定 logger 是否开启，打印的日志级别，还可以自定义 logger。
-
 // 自定义 Logger 需要实现 LoggerService 接口，或者继承 ConsoleLogger 然后重写部分方法。
-
 // 如果想在 Logger 注入一些 provider，就需要创建应用时设置 bufferLogs 为 true，然后用 app.useLogger(app.get(xxxLogger)) 来指定 Logger。
-
 // 你可以把这个自定义 Logger 封装到全局模块，或者动态模块里。
-
 // 当然，一般情况下，直接使用 Logger 就可以了。
